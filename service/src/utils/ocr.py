0cce03af537781414d0b1447a787895f1810fd91
Rutvik-C <rutvikchoudhari97@gmail.com>
2022-11-26 14:12:59 +0530

    command line args, bug fixes

--- service/src/utils/ocr.py
+++ service/src/utils/ocr.py
@@ -12,29 +12,33 @@ def doOCR(imgPath):
     subscriptionKey = config("OCR_SUBSCRIPTION_KEY")
     headers = {'Ocp-Apim-Subscription-Key': subscriptionKey, 'Content-Type': 'application/octet-stream'}
 
-    response = requests.post(endpoint, headers=headers, data=data)
-    response.raise_for_status()
-    operationUrl = response.headers["Operation-Location"]
+    try:
+        response = requests.post(endpoint, headers=headers, data=data)
+        response.raise_for_status()
+        operationUrl = response.headers["Operation-Location"]
 
-    print(f"INFO: OCR: Request submitted. Waiting.")
-    while True:
-        responseFinal = requests.get(operationUrl, headers=headers)
-        res = responseFinal.json()
+        print(f"INFO: OCR: Request submitted. Waiting.")
+        while True:
+            responseFinal = requests.get(operationUrl, headers=headers)
+            res = responseFinal.json()
 
-        if "analyzeResult" in res:
-            break
-        if "status" in res and res["status"] == "failed":
-            return ""
+            if "analyzeResult" in res:
+                break
+            if "status" in res and res["status"] == "failed":
+                return None, None
 
-        time.sleep(1)
+            time.sleep(1)
 
-    print(res)
-    lines = res["analyzeResult"]["readResults"][0]["lines"]
-    s = ""
-    add, count = 0, 0
-    for line in lines:
-        s += line["text"]
-        add += line["appearance"]["style"]["confidence"]
-        count += 1
+        lines = res["analyzeResult"]["readResults"][0]["lines"]
+        s = ""
+        add, count = 0, 0
+        for line in lines:
+            s += line["text"]
+            add += line["appearance"]["style"]["confidence"]
+            count += 1
 
-    return s.strip(), add / count
+        return s.strip(), add / count
+    
+    except Exception as e:
+        print(f"ERROR: OCR: Exception {e}")
+        return None, None
